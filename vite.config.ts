8eca8f77fc0d18dc942455eab040f0b5b54c3fbe
ayiezac <afacebu.ayie@gmail.com>
2025-09-03 13:44:35 +0800

    this should break

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+// import { resolve } from "node:path";
+
+export default {
+  // root: resolve(__dirname, "src"),
+  // build: {
+  //   outDir: "../dist",
+  // },
+  // server: {
+  //   port: 8080,
+  // },
+  // Optional: Silence Sass deprecation warnings. See note below.
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "import",
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+        ],
+      },
+    },
+  },
+};
